f28078b4c3bda8574425dbc29ac5e58126f3d85e
subhasish-santra <subhasishsantra78@gmail.com>
2025-03-25 02:57:22 +0530

    seven commit add a bottom navigation bar and app drawer and add many fragments and add a logo

---
+++ app/src/main/java/com/example/jis_lms/electronicsfragment.kt
@@ -0,0 +1,19 @@
+package com.example.jis_lms
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class electronicsfragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_electronicsfragment, container, false)
+    }
+
+}
