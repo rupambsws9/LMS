36e57e54b4a5aacd2ca0721c5f61f69850641470
Rupam Biswas <136965199+rupambsws9@users.noreply.github.com>
2025-03-26 11:04:21 +0530

    Revert "nine commit"

--- app/src/main/java/com/example/jis_lms/MainActivity.kt
+++ app/src/main/java/com/example/jis_lms/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import com.example.jis_lms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,37 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        replaceFragment(home_fragment())
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> replaceFragment(home_fragment())
-                R.id.payment -> replaceFragment(payment_fragment())
-                R.id.profile -> replaceFragment(profile_fragment())
-                R.id.lms -> replaceFragment(lms_fragment())
-                R.id.menu -> replaceFragment(menu_fragment())
-
-                else ->{
-
-                }
-            }
-            true
-
-        }
-
-
-    }
-
-    private fun replaceFragment(fragment:Fragment){
-        val fragmentmanager = supportFragmentManager
-        val fragmentTransction = fragmentmanager.beginTransaction()
-        fragmentTransction.replace(R.id.frameLayout , fragment)
-        fragmentTransction.commit()
 
     }
 
